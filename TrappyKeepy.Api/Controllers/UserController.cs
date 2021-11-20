a6dbc3c675b88e7cfe3649b10702892e73ca566a
jmg1138 <joshua@unblinking.io>
2021-11-20 14:28:25 -0800

    Started the first controller, the `UserController.cs` class. First endpoint is a GET request at the root of this controller to get all records. Haven't put any checks in place yet to limit this to only an admin role, that will come later.

---
+++ TrappyKeepy.Api/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrappyKeepy.Domain;
+using TrappyKeepy.Domain.Interfaces;
+
+
+namespace TrappyKeepy.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<List<User>>> ReadAll()
+        {
+            var users = await userService.ReadAll();
+            return Ok(users);
+        }
+    }
+}
