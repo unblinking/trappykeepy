de3aecfcc320190c5446ded993fb2e2809049b2a
jmg1138 <joshua@unblinking.io>
2021-11-22 13:31:04 -0800

    Changes to the UnitOfWork class so that when the transaction is being committed it will close the transaction.Connection as well as the base connection. Also, when the UnitOfWork is being disposed, if the transaction was still open, it will close the transaction.Connection after performing a rollback. Also some formatting applied (whitespace changes) in the file.

--- TrappyKeepy.Data/UnitOfWork.cs
+++ TrappyKeepy.Data/UnitOfWork.cs
@@ -11,40 +11,61 @@ namespace TrappyKeepy.Data
         private NpgsqlConnection connection;
         private NpgsqlTransaction? transaction;
         private bool disposed;
-        
 
-        public UnitOfWork(string connectionString, bool useTransaction){
+
+        public UnitOfWork(string connectionString, bool useTransaction)
+        {
             this.connectionString = connectionString;
             this.connection = new NpgsqlConnection(connectionString);
             this.connection.Open();
-            if (useTransaction){
+            if (useTransaction)
+            {
                 this.transaction = connection.BeginTransaction();
             }
         }
 
-        public UserRepository UserRepository{
-            get {
-                if (this.userRepository is null){
+        public UserRepository UserRepository
+        {
+            get
+            {
+                if (this.userRepository is null)
+                {
                     userRepository = new UserRepository(connection);
                 }
                 return userRepository;
             }
         }
 
-        public void Commit(){
-            if (transaction is not null){
+        public void Commit()
+        {
+            if (transaction is not null)
+            {
                 transaction.Commit();
+            }
+            if (transaction is not null && transaction.Connection is not null)
+            {
+                transaction.Connection.Close();
+            }
+            if (connection is not null)
+            {
                 connection.Close();
             }
         }
 
-        public void Rollback() {
-            if (transaction is not null){
+        public void Rollback()
+        {
+            if (transaction is not null)
+            {
                 transaction.Rollback();
+            }
+            if (transaction is not null && transaction.Connection is not null)
+            {
+                transaction.Connection.Close();
             }
         }
 
-        public void Dispose() {
+        public void Dispose()
+        {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
@@ -56,7 +77,11 @@ namespace TrappyKeepy.Data
                 // If we are in a transaction, but the connection wasn't closed,
                 // then something went wrong. The connection should be closed
                 // immediately after the transaction commit.
-                if (transaction is not null && transaction.Connection is not null) transaction.Rollback();
+                if (transaction is not null && transaction.Connection is not null)
+                {
+                    transaction.Rollback();
+                    transaction.Connection.Close();
+                }
                 if (connection is not null)
                 {
                     connection.Close();
