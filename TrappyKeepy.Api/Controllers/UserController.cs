e8b5dbb236b735d1413b10b549a191a1306b79b7
jmg1138 <joshua@unblinking.io>
2021-11-22 19:54:20 -0800

    Wrapped the UserController endpoints work in try/catch.

--- TrappyKeepy.Api/Controllers/UserController.cs
+++ TrappyKeepy.Api/Controllers/UserController.cs
@@ -22,20 +22,25 @@ namespace TrappyKeepy.Api.Controllers
         [HttpGet("")]
         public async Task<ActionResult> ReadAll()
         {
-            var serviceRequest = new UserServiceRequest();
-            var serviceResponse = await userService.ReadAll(serviceRequest);
-            var response = new ControllerResponse();
-            switch (serviceResponse.Outcome)
-            {
-                case OutcomeType.Error:
-                    response.Error();
-                    return StatusCode(500, response);
-                case OutcomeType.Fail:
-                    response.Fail(serviceResponse.ErrorMessage);
-                    return BadRequest(response);
-                case OutcomeType.Success:
-                    response.Success(serviceResponse.List);
-                    return Ok(response);
+            try {
+                var serviceRequest = new UserServiceRequest();
+                var serviceResponse = await userService.ReadAll(serviceRequest);
+                var response = new ControllerResponse();
+                switch (serviceResponse.Outcome)
+                {
+                    case OutcomeType.Error:
+                        response.Error();
+                        return StatusCode(500, response);
+                    case OutcomeType.Fail:
+                        response.Fail(serviceResponse.ErrorMessage);
+                        return BadRequest(response);
+                    case OutcomeType.Success:
+                        response.Success(serviceResponse.List);
+                        return Ok(response);
+                }
+            } catch (Exception) {
+                // TODO: Log exception somewhere?
+                return StatusCode(500);
             }
             // Default to error if unknown outcome from the service.
             return StatusCode(500);
@@ -44,20 +49,25 @@ namespace TrappyKeepy.Api.Controllers
         [HttpPost("")]
         public async Task<ActionResult> Create([FromBody] User user)
         {
-            var serviceRequest = new UserServiceRequest(user);
-            var serviceResponse = await userService.Create(serviceRequest);
-            var response = new ControllerResponse();
-            switch (serviceResponse.Outcome)
-            {
-                case OutcomeType.Error:
-                    response.Error();
-                    return StatusCode(500, response);
-                case OutcomeType.Fail:
-                    response.Fail(serviceResponse.ErrorMessage);
-                    return BadRequest(response);
-                case OutcomeType.Success:
-                    response.Success(serviceResponse.Id);
-                    return Ok(response);
+            try{
+                var serviceRequest = new UserServiceRequest(user);
+                var serviceResponse = await userService.Create(serviceRequest);
+                var response = new ControllerResponse();
+                switch (serviceResponse.Outcome)
+                {
+                    case OutcomeType.Error:
+                        response.Error();
+                        return StatusCode(500, response);
+                    case OutcomeType.Fail:
+                        response.Fail(serviceResponse.ErrorMessage);
+                        return BadRequest(response);
+                    case OutcomeType.Success:
+                        response.Success(serviceResponse.Id);
+                        return Ok(response);
+                }
+            } catch (Exception) {
+                // TODO: Log exception somewhere?
+                return StatusCode(500);
             }
             // Default to error if unknown outcome from the service.
             return StatusCode(500);
