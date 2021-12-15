dfdc704a9e38455fa1698eea738b3cad3953ffd9
jmg1138 <joshua@unblinking.io>
2021-12-14 23:22:03 -0800

    Added more seed methods to put data into the temp testing database for the e2e tests. Now have data in every database table. Various users, groups, users have memberships to different groups, documents/keepers, and permits issued for different documents to different groups or users.

--- TrappyKeepy.Test/End2End/SpawnyDb.cs
+++ TrappyKeepy.Test/End2End/SpawnyDb.cs
@@ -17,9 +17,25 @@ namespace TrappyKeepy.Test.End2End
 
         // Connection to use when running queries in the temporary testing database.
         private NpgsqlConnection _connectionUseTestDb;
+
+        // Seeded users in the temporary testing database.
+        // Can be handy to keep track of them here (e.g. we can issue authentication tokens for their user id).
+        // Keep in mind these are re-seeded/recycled/replaced with fresh users between every test.
         private User? _userAdmin;
         private User? _userManager;
         private User? _userBasic;
+
+        // Seeded keepers in the temporary testing database.
+        private Keeper? _keeperApiDll;
+        private Keeper? _keeperDataDll;
+        private Keeper? _keeperDomainDll;
+        private Keeper? _keeperServiceDll;
+        private Keeper? _keeperTestDll;
+
+        // Seeded groups in the temporary testing database.
+        private Group? _groupAdmins;
+        private Group? _groupManagers;
+        private Group? _groupBasics;
 
         public SpawnyDb()
         {
@@ -41,24 +57,59 @@ namespace TrappyKeepy.Test.End2End
         {
             // Drop any old temporary testing database, and create a fresh one.
             await _connectionCreateTestDb.OpenAsync();
-            await Drop();
-            await Create();
+            await DropDb();
+            await CreateDb();
             await _connectionCreateTestDb.CloseAsync();
             await _connectionCreateTestDb.DisposeAsync();
 
             // Seed test data into the temporary testing database for the next e2e test.
             await _connectionUseTestDb.OpenAsync();
-            await SeedAdminUser();
-            await SeedManagerUser();
-            await SeedBasicUser();
+            await SeedUserAdmin();
+            await SeedUserManager();
+            await SeedUserBasic();
             await SeedKeeperApiDll();
             await SeedKeeperDataDll();
             await SeedKeeperDomainDll();
             await SeedKeeperServiceDll();
             await SeedKeeperTestDll();
+            await SeedGroupAdmins();
+            await SeedGroupManagers();
+            await SeedGroupBasics();
+            await SeedMembershipAdmin();
+            await SeedMembershipManager();
+            await SeedMembershipBasic();
+            await SeedPermitApiDllAdminUser();
+            await SeedPermitDataDllAdminsGroup();
+            await SeedPermitDomainDllManagerUser();
+            await SeedPermitServiceDllManagersGroup();
+            await SeedPermitTestDllBasicUser();
             await _connectionUseTestDb.CloseAsync();
             await _connectionUseTestDb.DisposeAsync();
         }
+
+        private async Task DropDb()
+        {
+            using (var command = new NpgsqlCommand())
+            {
+                command.CommandText = $"DROP DATABASE IF EXISTS {_testDbName};";
+                command.Connection = _connectionCreateTestDb;
+                await command.PrepareAsync();
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        private async Task CreateDb()
+        {
+            using (var command = new NpgsqlCommand())
+            {
+                command.CommandText = $"CREATE DATABASE {_testDbName} WITH TEMPLATE keepydb OWNER dbowner;";
+                command.Connection = _connectionCreateTestDb;
+                await command.PrepareAsync();
+                await command.ExecuteNonQueryAsync();
+            }
+        }
+
+        #region PUBLIC METHODS
 
         public string AuthenticateAdmin()
         {
@@ -93,33 +144,15 @@ namespace TrappyKeepy.Test.End2End
             return token;
         }
 
-        private async Task Drop()
-        {
-            using (var command = new NpgsqlCommand())
-            {
-                command.CommandText = $"DROP DATABASE IF EXISTS {_testDbName};";
-                command.Connection = _connectionCreateTestDb;
-                await command.PrepareAsync();
-                await command.ExecuteNonQueryAsync();
-            }
-        }
+        #endregion PUBLIC METHODS
 
-        private async Task Create()
-        {
-            using (var command = new NpgsqlCommand())
-            {
-                command.CommandText = $"CREATE DATABASE {_testDbName} WITH TEMPLATE keepydb OWNER dbowner;";
-                command.Connection = _connectionCreateTestDb;
-                await command.PrepareAsync();
-                await command.ExecuteNonQueryAsync();
-            }
-        }
+        #region USERS
 
-        private async Task SeedAdminUser()
+        private async Task<User> SeedUser(string name, string password, string email, string role)
         {
             using (var command = new NpgsqlCommand())
             {
-                command.CommandText = "SELECT * FROM tk.users_create('admin', 'passwordadmin', 'admin@trappykeepy.com', 'admin');";
+                command.CommandText = $"SELECT * FROM tk.users_create('{name}', '{password}', '{email}', '{role}');";
                 command.Connection = _connectionUseTestDb;
                 await command.PrepareAsync();
                 var reader = await command.ExecuteReaderAsync();
@@ -130,55 +163,36 @@ namespace TrappyKeepy.Test.End2End
                     newUser = map.User(reader);
                 }
                 await reader.CloseAsync();
-                _userAdmin = newUser;
+                return newUser;
             }
         }
 
-        private async Task SeedManagerUser()
+        private async Task SeedUserAdmin()
         {
-            using (var command = new NpgsqlCommand())
-            {
-                command.CommandText = "SELECT * FROM tk.users_create('manager', 'passwordmanager', 'manager@trappykeepy.com', 'manager');";
-                command.Connection = _connectionUseTestDb;
-                await command.PrepareAsync();
-                var reader = await command.ExecuteReaderAsync();
-                var userManager = new User();
-                while (await reader.ReadAsync())
-                {
-                    var map = new PgsqlReaderMap();
-                    userManager = map.User(reader);
-                }
-                await reader.CloseAsync();
-                _userManager = userManager;
-            }
+            _userAdmin = await SeedUser("admin", "passwordadmin", "admin@trappykeepy.com", "admin");
         }
 
-        private async Task SeedBasicUser()
+        private async Task SeedUserManager()
         {
-            using (var command = new NpgsqlCommand())
-            {
-                command.CommandText = "SELECT * FROM tk.users_create('basic', 'passwordbasic', 'basic@trappykeepy.com', 'basic');";
-                command.Connection = _connectionUseTestDb;
-                await command.PrepareAsync();
-                var reader = await command.ExecuteReaderAsync();
-                var userBasic = new User();
-                while (await reader.ReadAsync())
-                {
-                    var map = new PgsqlReaderMap();
-                    userBasic = map.User(reader);
-                }
-                await reader.CloseAsync();
-                _userBasic = userBasic;
-            }
+            _userManager = await SeedUser("manager", "passwordmanager", "manager@trappykeepy.com", "manager");
         }
 
-        private async Task SeedKeeper(string filename)
+        private async Task SeedUserBasic()
+        {
+            _userBasic = await SeedUser("basic", "passwordbasic", "basic@trappykeepy.com", "basic");
+        }
+
+        #endregion USERS
+
+        #region KEEPERS
+
+        private async Task<Keeper> SeedKeeper(string filename)
         {
             var contentType = "application/octet-stream";
             byte[] binaryData = await File.ReadAllBytesAsync(filename);
             var adminId = Guid.Empty;
             if (_userAdmin?.Id is not null && _userAdmin.Id != Guid.Empty) adminId = _userAdmin.Id;
-            var keeperId = Guid.Empty;
+            Keeper keeper = new Keeper();
             using (var command = new NpgsqlCommand())
             {
                 command.CommandText = $"SELECT * FROM tk.keepers_create('{filename}', '{contentType}', '{adminId}', 'The {filename} file.', 'Files');";
@@ -188,14 +202,14 @@ namespace TrappyKeepy.Test.End2End
                 while (await reader.ReadAsync())
                 {
                     var map = new PgsqlReaderMap();
-                    var keeper = map.Keeper(reader);
-                    keeperId = keeper.Id;
+                    keeper = map.Keeper(reader);
                 }
                 await reader.CloseAsync();
             }
+            if (keeper.Id == Guid.Empty) throw new Exception("Could not seed keeper.");
             using (var command = new NpgsqlCommand())
             {
-                command.CommandText = $"SELECT * FROM tk.filedatas_create('{keeperId}', :binary_data );";
+                command.CommandText = $"SELECT * FROM tk.filedatas_create('{keeper.Id}', :binary_data );";
                 var npgsqlParameter = new NpgsqlParameter("binary_data", NpgsqlTypes.NpgsqlDbType.Bytea);
                 npgsqlParameter.Value = binaryData;
                 command.Parameters.Add(npgsqlParameter);
@@ -203,31 +217,183 @@ namespace TrappyKeepy.Test.End2End
                 await command.PrepareAsync();
                 await command.ExecuteScalarAsync();
             }
+            return keeper;
         }
 
         private async Task SeedKeeperApiDll()
         {
-            await SeedKeeper("TrappyKeepy.Api.dll");
+            _keeperApiDll = await SeedKeeper("TrappyKeepy.Api.dll");
         }
 
         private async Task SeedKeeperDataDll()
         {
-            await SeedKeeper("TrappyKeepy.Data.dll");
+            _keeperDataDll = await SeedKeeper("TrappyKeepy.Data.dll");
         }
 
         private async Task SeedKeeperDomainDll()
         {
-            await SeedKeeper("TrappyKeepy.Domain.dll");
+            _keeperDomainDll = await SeedKeeper("TrappyKeepy.Domain.dll");
         }
 
         private async Task SeedKeeperServiceDll()
         {
-            await SeedKeeper("TrappyKeepy.Service.dll");
+            _keeperServiceDll = await SeedKeeper("TrappyKeepy.Service.dll");
         }
 
         private async Task SeedKeeperTestDll()
         {
-            await SeedKeeper("TrappyKeepy.Test.dll");
+            _keeperTestDll = await SeedKeeper("TrappyKeepy.Test.dll");
         }
+
+        #endregion KEEPERS
+
+        #region GROUPS
+
+        private async Task<Group> SeedGroup(string name, string description)
+        {
+            using (var command = new NpgsqlCommand())
+            {
+                command.CommandText = $"SELECT * FROM tk.groups_create('{name}', '{description}');";
+                command.Connection = _connectionUseTestDb;
+                await command.PrepareAsync();
+                var reader = await command.ExecuteReaderAsync();
+                var newGroup = new Group();
+                while (await reader.ReadAsync())
+                {
+                    var map = new PgsqlReaderMap();
+                    newGroup = map.Group(reader);
+                }
+                await reader.CloseAsync();
+                return newGroup;
+            }
+        }
+
+        private async Task SeedGroupAdmins()
+        {
+            _groupAdmins = await SeedGroup("Admins", "Group for all administrator users.");
+        }
+
+        private async Task SeedGroupManagers()
+        {
+            _groupManagers = await SeedGroup("Managers", "Group for all manager users.");
+        }
+
+        private async Task SeedGroupBasics()
+        {
+            _groupBasics = await SeedGroup("Basics", "Group for all basic users.");
+        }
+
+        #endregion GROUPS
+
+        #region MEMBERSHIPS
+
+        private async Task<Membership> SeedMembership(Guid groupId, Guid userId)
+        {
+            using (var command = new NpgsqlCommand())
+            {
+                command.CommandText = $"SELECT * FROM tk.memberships_create('{groupId}', '{userId}');";
+                command.Connection = _connectionUseTestDb;
+                await command.PrepareAsync();
+                var reader = await command.ExecuteReaderAsync();
+                var newMembership = new Membership();
+                while (await reader.ReadAsync())
+                {
+                    var map = new PgsqlReaderMap();
+                    newMembership = map.Membership(reader);
+                }
+                await reader.CloseAsync();
+                return newMembership;
+            }
+        }
+
+        private async Task SeedMembershipAdmin()
+        {
+            if (_groupAdmins is null || _userAdmin is null)
+            {
+                throw new Exception("Could not seed membership.");
+            }
+            await SeedMembership(_groupAdmins.Id, _userAdmin.Id);
+        }
+
+        private async Task SeedMembershipManager()
+        {
+            if (_groupManagers is null || _userManager is null)
+            {
+                throw new Exception("Could not seed membership.");
+            }
+            await SeedMembership(_groupManagers.Id, _userManager.Id);
+        }
+
+        private async Task SeedMembershipBasic()
+        {
+            if (_groupBasics is null || _userBasic is null)
+            {
+                throw new Exception("Could not seed membership.");
+            }
+            await SeedMembership(_groupBasics.Id, _userBasic.Id);
+        }
+
+        #endregion MEMBERSHIPS
+
+        #region PERMITS
+
+        private async Task<Permit> SeedPermit(Guid keeperId, Guid? userId, Guid? groupId)
+        {
+            using (var command = new NpgsqlCommand())
+            {
+                command.CommandText = $"SELECT * FROM tk.permits_create('{keeperId}'";
+
+                if (userId is not null) command.CommandText += $", '{userId}'";
+                else command.CommandText += $", null";
+
+                if (groupId is not null) command.CommandText += $", '{groupId}'";
+                else command.CommandText += $", null";
+
+                command.CommandText += ");";
+                command.Connection = _connectionUseTestDb;
+                await command.PrepareAsync();
+                var reader = await command.ExecuteReaderAsync();
+                var newPermit = new Permit();
+                while (await reader.ReadAsync())
+                {
+                    var map = new PgsqlReaderMap();
+                    newPermit = map.Permit(reader);
+                }
+                await reader.CloseAsync();
+                return newPermit;
+            }
+        }
+
+        private async Task SeedPermitApiDllAdminUser()
+        {
+            if (_keeperApiDll is null || _userAdmin is null) throw new Exception("Could not create permit.");
+            await SeedPermit(_keeperApiDll.Id, _userAdmin.Id, null);
+        }
+
+        private async Task SeedPermitDataDllAdminsGroup()
+        {
+            if (_keeperDataDll is null || _groupAdmins is null) throw new Exception("Could not create permit.");
+            await SeedPermit(_keeperDataDll.Id, null, _groupAdmins.Id);
+        }
+
+        private async Task SeedPermitDomainDllManagerUser()
+        {
+            if (_keeperDomainDll is null || _userManager is null) throw new Exception("Could not create permit.");
+            await SeedPermit(_keeperDomainDll.Id, _userManager.Id, null);
+        }
+
+        private async Task SeedPermitServiceDllManagersGroup()
+        {
+            if (_keeperServiceDll is null || _groupManagers is null) throw new Exception("Could not create permit.");
+            await SeedPermit(_keeperServiceDll.Id, null, _groupManagers.Id);
+        }
+
+        private async Task SeedPermitTestDllBasicUser()
+        {
+            if (_keeperTestDll is null || _userBasic is null) throw new Exception("Could not create permit.");
+            await SeedPermit(_keeperTestDll.Id, _userBasic.Id, null);
+        }
+
+        #endregion PERMITS
     }
 }
